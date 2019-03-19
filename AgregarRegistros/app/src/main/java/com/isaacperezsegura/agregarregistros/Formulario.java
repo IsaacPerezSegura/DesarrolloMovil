e590b86d0471acbc048fcf5c6949f12c670521b2
IsaacPerezSegura <15240485@itleon.edu.mx>
2019-03-19 08:17:00 -0600

    Chi Cheñol :v

--- AgregarRegistros/app/src/main/java/com/isaacperezsegura/agregarregistros/Formulario.java
+++ AgregarRegistros/app/src/main/java/com/isaacperezsegura/agregarregistros/Formulario.java
@@ -5,23 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import java.util.ArrayList;
 
 public class Formulario extends AppCompatActivity {
-    private Alumno alumno;
+    private ArrayList<Alumno> alumno;
     private EditText textView;
+    private Intent main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
         textView = (EditText) findViewById(R.id.input);
+        main = new Intent(this, MainActivity.class);
+        if(getIntent().hasExtra("lista_alumnos")) {
+            alumno = (ArrayList<Alumno>) getIntent().getSerializableExtra("lista_alumnos");
+        }
     }
     public void registrar(View view){
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("informacion", textView.getText().toString());
-        startActivity(intent);
+        alumno.add(new Alumno(textView.getText().toString()));
+        main.putExtra("lista_alumnos", alumno);
+        startActivity(main);
+    }
+    public void cancelar(View view){
+        main.putExtra("lista_alumnos", alumno);
+        startActivity(main);
     }
 }
--- AgregarRegistros/app/src/main/java/com/isaacperezsegura/agregarregistros/MainActivity.java
+++ AgregarRegistros/app/src/main/java/com/isaacperezsegura/agregarregistros/MainActivity.java
@@ -4,22 +4,44 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private TextView show;
+    private EditText busqueda;
+    private ArrayList <Alumno> listaAlumno;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         show = (TextView) findViewById(R.id.show);
-        show.setText(getIntent().getStringExtra("informacion"));
+        busqueda = (EditText) findViewById(R.id.busqueda);
+        listaAlumno = new ArrayList();
+        if(getIntent().hasExtra("lista_alumnos")){
+            this.listaAlumno = (ArrayList<Alumno>) getIntent().getExtras().getSerializable("lista_alumnos");
+        }
+        muestra_alumnos();
     }
-
-    public void changeActivity (View view){
-        Intent intent = new Intent(this, Formulario.class);
+    public void agregar(View view){
+        intent = new Intent(this, Formulario.class);
+        intent.putExtra("lista_alumnos",listaAlumno);
         startActivity(intent);
+    }
+    private void muestra_alumnos(){
+        //Validar si hay personas registradas
+        if(listaAlumno.isEmpty()) {
+            show.setText("No hay alumnos");
+        }else{
+            String lista = "";
+            for (Alumno temp:listaAlumno){
+                lista = lista + temp.getNombre()+"\n";
+            }
+            show.setText(lista);
+        }
     }
 }
